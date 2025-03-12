4c9d2dbf9b99843a98b36f911cd609c8fec8cf18
Ivan Bilchenko <ivanbilchenko@icloud.com>
2025-03-12 15:04:14 +0200

    CA-002 Implement Authentication remote data source

---
+++ lib/src/core/constants/constants.dart
@@ -0,0 +1,1 @@
+const kBaseUrl = '67c2e7411851890165ad9708.mockapi.io';
---
+++ test/src/features/data/data_souces/authentication_remote_data_source_test.dart
@@ -0,0 +1,108 @@
+import 'dart:convert';
+
+import 'package:flutter_clean_architecture_example/src/core/constants/constants.dart';
+import 'package:flutter_clean_architecture_example/src/core/error/exceptions.dart';
+import 'package:flutter_clean_architecture_example/src/features/data/data_sources/authentication_remote_data_source.dart';
+import 'package:flutter_clean_architecture_example/src/features/data/models/user_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:http/http.dart' as http;
+
+class MockClient extends Mock implements http.Client {}
+
+void main() {
+  late http.Client client;
+  late AuthenticationRemoteDataSource remoteDataSource;
+
+  setUp(() {
+    client = MockClient();
+    remoteDataSource = AuthenticationRemoteDataSourceImpl(client: client);
+    registerFallbackValue(Uri());
+  });
+
+  group('createUser', () {
+    const tCreatedAt = 'createdAt';
+    const tName = 'name';
+    const tAvatar = 'avatar';
+    final tBody = jsonEncode({
+      'createdAt': tCreatedAt,
+      'name': tName,
+      'avatar': tAvatar,
+    });
+
+    test('should complete successfully when the status code is 200 or 201',
+        () async {
+      when(() => client.post(any(), body: any(named: 'body'))).thenAnswer(
+          (_) async => http.Response('User created successfully', 201));
+
+      final methodCall = remoteDataSource.createUser;
+
+      expect(methodCall(createdAt: tCreatedAt, name: tName, avatar: tAvatar),
+          completes);
+
+      verify(() => client.post(
+            Uri.https(kBaseUrl, kCreateUserEndpoint),
+            body: tBody,
+          )).called(1);
+
+      verifyNoMoreInteractions(client);
+    });
+
+    test('should throw [ApiException] when the status code is not 200 or 201',
+        () async {
+      when(() => client.post(any(), body: any(named: 'body')))
+          .thenAnswer((_) async => http.Response('Invalid email address', 400));
+
+      final methodCall = remoteDataSource.createUser;
+
+      expect(
+          () async =>
+              methodCall(createdAt: tCreatedAt, name: tName, avatar: tAvatar),
+          throwsA(const ApiException(
+              message: 'Invalid email address', statusCode: 400)));
+
+      verify(() => client.post(
+            Uri.https(kBaseUrl, kCreateUserEndpoint),
+            body: tBody,
+          )).called(1);
+
+      verifyNoMoreInteractions(client);
+    });
+  });
+
+  group('getUsers', () {
+    const tUsers = [UserModel.empty()];
+    final tResponse = jsonEncode([tUsers.first.toMap()]);
+
+    test('should return [List<User>] when the status code is 200', () async {
+      when(() => client.get(any()))
+          .thenAnswer((_) async => http.Response(tResponse, 200));
+
+      final result = await remoteDataSource.getUsers();
+
+      expect(result, equals(tUsers));
+
+      verify(() => client.get(Uri.https(kBaseUrl, kGetUsersEndpoint)))
+          .called(1);
+
+      verifyNoMoreInteractions(client);
+    });
+
+    test('should throw [ApiException] when the status code is not 200 or 201',
+        () async {
+      const tMessage = 'Server down';
+      when(() => client.get(any()))
+          .thenAnswer((_) async => http.Response(tMessage, 500));
+
+      final methodCall = remoteDataSource.getUsers;
+
+      expect(() async => methodCall(),
+          throwsA(const ApiException(message: tMessage, statusCode: 500)));
+
+      verify(() => client.get(Uri.https(kBaseUrl, kGetUsersEndpoint)))
+          .called(1);
+
+      verifyNoMoreInteractions(client);
+    });
+  });
+}
